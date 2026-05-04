308f610e20c057e3c1d2c8003b6545aeec264334
ggarabs <ggarabetti0607@gmail.com>
2026-05-04 16:03:24 -0300

    feat: add entity schema in the wire and entrypoint validation

--- src/hairstyle_api/diplomat/http_server.clj
+++ src/hairstyle_api/diplomat/http_server.clj
@@ -5,19 +5,41 @@
    [io.pedestal.http.body-params :refer [body-params]]
    [io.pedestal.http.route :refer [expand-routes]]
    [hairstyle-api.controllers.hairstyle :as controllers.hairstyle]
-   [hairstyle-api.wire.in.hairstyle :as wire.in.hairstyle]
    [hairstyle-api.db.config :refer [conn]]
-   [hairstyle-api.db.hairstyle :as db.hairstyle]))
+   [hairstyle-api.db.hairstyle :as db.hairstyle]
+   [hairstyle-api.wire.in.hairstyle :as wire.in]
+   [io.pedestal.interceptor.chain :as chain]
+   [schema.core :as s]))
 
 (defn inject-db [datomic-db]
   {:name  ::inject-db
    :enter (fn [context]
             (assoc-in context [:request :datomic] datomic-db))})
 
+(defn validate-body [schema]
+  {:name ::validate-body
+   :enter
+   (fn [context]
+     (let [body (or (get-in context [:request :json-params]) {})
+           errors (s/check schema body)]
+       (if errors
+         (-> context
+             (assoc :response {:status 400
+                               :body {:errors errors}}) 
+             chain/terminate) 
+         context)))})
+
 (def common-interceptors
   [http/log-request
    (body-params)
    http/json-body
+   (inject-db (db.hairstyle/->DatomicDB conn))])
+
+(def body-interceptors
+  [http/log-request
+   (body-params)
+   http/json-body
+   (validate-body wire.in/hairstyle)
    (inject-db (db.hairstyle/->DatomicDB conn))])
 
 (defn current-version [_]
@@ -29,33 +51,32 @@
 
 (defn get-hairstyle-by-id [req]
   (let [string-id (get-in req [:path-params :id])
-        converted-id (wire.in.hairstyle/id-string->long string-id)]
+        converted-id (wire.in/id-string->long string-id)]
     (controllers.hairstyle/get-by-id converted-id req)))
 
 (defn insert-hairstyle [req]
   (let [hairstyle-details (:json-params req)
-        details-namespaced (wire.in.hairstyle/external->domain hairstyle-details)]
+        details-namespaced (wire.in/external->domain hairstyle-details)]
     (controllers.hairstyle/insert! details-namespaced req)))
 
 (defn delete-hairstyle [req]
   (let [string-id (get-in req [:path-params :id])
-        converted-id (wire.in.hairstyle/id-string->long string-id)]
+        converted-id (wire.in/id-string->long string-id)]
     (controllers.hairstyle/delete! converted-id req)))
 
 (defn put-hairstyle [req]
   (let [string-id (get-in req [:path-params :id]) 
-        converted-id (wire.in.hairstyle/id-string->long string-id)
+        converted-id (wire.in/id-string->long string-id)
         hairstyle-details (:json-params req)
-        details-namespaced (wire.in.hairstyle/external->domain hairstyle-details)]
+        details-namespaced (wire.in/external->domain hairstyle-details)]
     (controllers.hairstyle/update! converted-id details-namespaced req)))
 
 (defn patch-hairstyle [req]
   (let [string-id (get-in req [:path-params :id]) 
-        converted-id (wire.in.hairstyle/id-string->long string-id)
+        converted-id (wire.in/id-string->long string-id)
         hairstyle-details (:json-params req)
-        details-namespaced (wire.in.hairstyle/external->domain hairstyle-details)]
+        details-namespaced (wire.in/external->domain hairstyle-details)]
     (controllers.hairstyle/patch! converted-id details-namespaced req)))
-
 
 (def default-routes
   #{["/api/version"
@@ -69,7 +90,7 @@
                 get-hairstyles)
      :route-name :get-haistyles]
     ["/api/hairstyle"
-     :post (conj common-interceptors
+     :post (conj body-interceptors
                  insert-hairstyle)
      :route-name :post-haistyle]
     ["/api/hairstyle/:id"
--- src/hairstyle_api/wire/in/hairstyle.clj
+++ src/hairstyle_api/wire/in/hairstyle.clj
@@ -1,4 +1,15 @@
-(ns hairstyle-api.wire.in.hairstyle)
+(ns hairstyle-api.wire.in.hairstyle
+  (:require [schema.core :as s]))
+
+(s/defschema hairstyle
+  {:name s/Str
+   (s/optional-key :slug) s/Str
+   :description s/Str
+   :texture [s/Str]
+   :length s/Str
+   :main-image s/Str
+   (s/optional-key :gallery) [s/Str]
+   (s/optional-key :tags) [s/Str]})
 
 (defn external->domain [req]
   (update-keys req #(keyword "hairstyle" (name %))))

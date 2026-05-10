626b20877ed269989375298a266094e8a1cb9270
ggarabs <ggarabetti0607@gmail.com>
2026-05-09 23:43:31 -0300

    refactor: remove unused files and functions

--- src/hairstyle_api/config.clj
+++
@@ -1,1 +0,0 @@
-(ns hairstyle-api.config)
--- src/hairstyle_api/wire/out/hairstyle.clj
+++ src/hairstyle_api/wire/out/hairstyle.clj
@@ -1,14 +1,6 @@
 (ns hairstyle-api.wire.out.hairstyle
   (:require [schema.core :as s]))
 
-(defn domain->external [entity]
-  (reduce-kv (fn [m k v]
-               (let [new-key (if (keyword? k) (name k) (str k))
-                     final-key (if (= new-key "id") "id" new-key)]
-                 (assoc m final-key (if (set? v) (vec v) v))))
-             {}
-             entity))
-
 (s/defschema Hairstyle
   {(s/required-key "id") s/Num
    (s/required-key "name") s/Str
